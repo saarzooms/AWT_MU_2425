dbeccdeb9b0e2af2871674e8585ba74d5a10e748
Shamsaagazarzoo Alam <arzoo.rkcet@gmail.com>
2025-02-19 08:59:29 +0530

    update with error module

--- node-express-app/index.js
+++ node-express-app/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const errors = require("./src/utils/errors");
 const staff = require("./src/routes/staff.routes");
+const student = require("./src/routes/student.routes");
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(errors.errorHandler);
 app.use("/staff", staff);
+app.use("/student", student);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 //17,13,18 present
